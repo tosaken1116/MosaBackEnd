45fef1d93c34f38b657e5ca82effe5245816871a
tosaken <v-ntosa@alterbooth.net>
2023-06-03 14:09:18 +0900

    add:rustのhello worldエンドポイントの追加

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world! with rust");
+use actix_web::{get,web,App,HttpResponse,HttpServer,Responder};
+use serde::{Serialize};
+
+#[derive(Serialize)]
+pub struct Response{
+    pub message:String
+}
+#[get("/hello")]
+async fn hello_world()-> impl Responder{
+    let response = Response{
+        message:"Hello world".to_string()
+    };
+    HttpResponse::Ok().json(response)
+}
+
+#[actix_web::main]
+async fn main()-> std::io::Result<()>{
+    HttpServer::new(|| App::new().service(hello_world).default_service(web::route()))
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
