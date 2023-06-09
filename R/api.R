96e76de90f3c86d28842e19baf6a1a86ba5f41b8
tosaken <v-ntosa@alterbooth.net>
2023-06-10 01:47:15 +0900

    add:db接続 データポストの実装 #36

--- R/api.R
+++ R/api.R
@@ -1,4 +1,20 @@
-#* @get /hello
-function(){
-   "Hello World"
+require("RPostgreSQL")
+require("dotenv")
+
+load_dot_env(".env")
+
+con <- dbConnect(
+   PostgreSQL(),
+   host = Sys.getenv("POSTGRES_HOST"),
+   port = Sys.getenv("POSTGRES_PORT"),
+   dbname = Sys.getenv("POSTGRES_DB"),
+   user = Sys.getenv("POSTGRES_USER"),
+   password = Sys.getenv("POSTGRES_PASSWORD")
+)
+
+
+#* @post /favorite
+function(user_id,post_id){
+dataset <-dbGetQuery(con,"INSERT INTO LIKES (user_id,posts_id) VALUES ($1,$2)",c(user_id,post_id))
+   dataset
 }
