73d85e993c7419e5b2c27288b2df5765259b114e
yamato0211 <kumayama0211@gmail.com>
2023-06-16 20:29:23 +0900

    Revert "add: いいね追加のエンドポイント作成"
    This reverts commit 69a63054647369701161a5d6517220f716adcc59.

--- cs/api/Program.cs
+++ cs/api/Program.cs
@@ -1,23 +1,5 @@
-using Microsoft.AspNetCore.Mvc;
-
-var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://0.0.0.0:8080");
-builder.Services.AddScoped<LikeService>();
-builder.Services.AddControllers().AddJsonOptions(options =>
-{
-    options.JsonSerializerOptions.Converters.Add(new GuidJsonConverter());
-});
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-        policy =>
-        {
-            policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
-        });
-});
-
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
