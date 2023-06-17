dc1d1f2b9a1fa8ff5dbecfeb828f67ca6e0e7e33
root <root@DESKTOP-ST3A33L.localdomain>
2023-06-17 15:36:55 +0900

    add:group編集エンドポイント

--- ruby/config/routes.rb
+++ ruby/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "hello/view", to: "hello#view"
+  get "group/:id", to: "group#show"
+  put 'group/:id', to: 'group#update'
   # Defines the root path route ("/")
   # root "articles#index"
 end
